ea811fe315916db92821fb4069f0763cd29b385d
demon962 <166789915+demon962@users.noreply.github.com>
2024-10-27 16:08:31 +0500

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -1,25 +1,33 @@
 class House:
     houses_history = []
-    def __init__(self, *args):
-        self.name = args[0]
-        self.address = args[1]
 
-    @classmethod
-    def new(cls, *args):
-        house_name = args[0]
-        cls.houses_history.append(house_name)
-        return cls(*args)
-    def __del__(self):
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.add_to_history(name)
+
+    def add_to_history(self, name):
+        House.houses_history.append(name)
+
+    def delete(self):
         print(f"{self.name} снесён, но он останется в истории")
 
+    def __del__(self):
+        self.delete()
+
+
 if __name__ == "__main__":
+    h1 = House('ЖК Эльбрус', 10)
+    print(House.houses_history)
 
-    house1 = House.new('Дом1', 'ул. Прима')
-    house2 = House.new('Дом2', 'ул. Секунда')
-    house3 = House.new('Дом3', 'ул. Терция')
+    h2 = House('ЖК Акация', 20)
+    print(House.houses_history)
 
-    print("История созданных домов:", House.houses_history)
+    h3 = House('ЖК Матрёшки', 20)
+    print(House.houses_history)
+    # Удаление объектов
+    del h2
+    del h3
 
-    del house2
-
-    print("История созданных домов после удаления:", House.houses_history)
+    print(
+        House.houses_history) 
